1459fa705736b5d0a15bd01afbe60b6bef73db01
anydayr <annyrondon491@gmail.com>
2025-06-30 20:24:56 -0300

    refactor: delete component

--- src/modules/authentication/pages/Login.tsx
+++ src/modules/authentication/pages/Login.tsx
@@ -1,7 +1,6 @@
-import { Button } from "@/components/ui/button";
 
 const LoginPage = () => {
-  return <div><Button variant="default">Login</Button></div>;
+  return <div>Login</div>;
 };
 
 export default LoginPage;
