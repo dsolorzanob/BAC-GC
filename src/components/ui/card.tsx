17fdab4437c1ec49746a9b18e0ffc786901a767e
anydayr <annyrondon491@gmail.com>
2025-06-30 23:33:44 -0300

    style: delete border

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6",
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl py-6",
         "shadow-[0px_1px_5px_0px_rgba(0,0,0,0.2)]",
         className
       )}
