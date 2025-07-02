919024db87ded2087428aa27b20320c94d155e7c
anydayr <annyrondon491@gmail.com>
2025-07-02 11:26:27 -0300

    feat: create form files

---
+++ src/modules/authentication/containers/EnterEmailForm.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/ui/card";
+
+
+const EnterEmailForm = () => {
+  return (
+    <Card className="w-full">
+      <h1>Reset Password</h1>
+    </Card>
+  );
+};
+
+export default EnterEmailForm;
