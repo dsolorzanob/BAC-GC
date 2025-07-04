f8df80cf1df7799b5fadcc771e57916779e6dc54
anydayr <annyrondon491@gmail.com>
2025-07-04 13:11:49 -0300

    refactor: change route

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { MainLayout } from "@/components/layouts/MainLayout";
-import ResetPasswordPage from "@/modules/authentication/pages/ResetPassword";
 import SignUpPage from "@/modules/authentication/pages/SignUp";
 import LoginPage from "@/modules/authentication/pages/Login";
 import NewPasswordPage from "@/modules/authentication/pages/NewPassword";
+import SendEmailPage from "@/modules/authentication/pages/ResetPassword";
 
 export const AppRouter = () => {
   return (
@@ -12,7 +12,7 @@ export const AppRouter = () => {
         {/* Layout de autenticación para páginas de login/registro */}
         <Route path="/" >
           <Route index element={<LoginPage />} />
-          <Route path="reset-password/enter-email" element={<ResetPasswordPage />} />
+          <Route path="reset-password/send-email" element={<SendEmailPage />} />
           <Route path="reset-password/new-password" element={<NewPasswordPage />} />
           <Route path="signup" element={<SignUpPage />} />
         </Route>
