4589b8000a906a41ae8849e0cb6de3af5a59eeec
anydayr <annyrondon491@gmail.com>
2025-07-04 13:48:16 -0300

    feat: create otp page

---
+++ src/modules/authentication/pages/OTP.tsx
@@ -0,0 +1,12 @@
+import { ResetPasswordLayout } from "../layouts/ResetPasswordLayout";
+import OTPForm from "../containers/OTPForm";
+
+const OTPPage = () => {
+  return (
+    <ResetPasswordLayout>
+      <OTPForm />
+    </ResetPasswordLayout>
+  );
+};
+
+export default OTPPage;
