45ac90cc336d3385a3d0c27686c42b0d586d53d7
anydayr <annyrondon491@gmail.com>
2025-07-04 13:39:21 -0300

    refactor: redefine zod schema and add new validation

--- src/modules/authentication/constants/new-password-schema.ts
+++ src/modules/authentication/constants/new-password-schema.ts
@@ -1,7 +1,10 @@
 import { z } from "zod";
 
 export const newPasswordSchema = z.object({
-  password: z.string().min(8),
-  confirmPassword: z.string().min(8),
+  password: z.string().min(8, { message: "La contraseña debe tener al menos 8 caracteres" }),
+  confirmPassword: z.string().min(8, { message: "La contraseña debe tener al menos 8 caracteres" }),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Las contraseñas no coinciden",
+  path: ["confirmPassword"], // Esto hace que el error aparezca en el campo confirmPassword
 });
 
