c3a60690ffeff7a9b2e0d06274c0e28f3119f504
anydayr <annyrondon491@gmail.com>
2025-08-18 07:02:23 -0300

    style: z-index layout

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,27 +1,33 @@
-import { Outlet } from "react-router-dom";
-import { AppSidebar } from "./AppSidebar";
-import { Header } from "./Header";
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { ThemeSwitch } from "./SwitchTheme";
-import { SearchSidebar } from "./SearchSidebar";
+import { Outlet } from 'react-router-dom';
+import { AppSidebar } from './AppSidebar';
+import { Header } from './Header';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { ThemeSwitch } from './SwitchTheme';
+import { SearchSidebar } from './SearchSidebar';
 
 export function MainLayout() {
   return (
     <SidebarProvider defaultOpen={true}>
-      <div className="flex h-screen w-full">
-        {/* Sidebar izquierdo */}
-        <AppSidebar />
+      <div className="flex h-screen w-full relative">
+        {/* Sidebar izquierdo - con z-index alto */}
+        <div className="relative z-50">
+          <AppSidebar />
+        </div>
+
         {/* Contenido principal */}
-        <div className="flex-1 flex flex-col">
-          {/* Header arriba */}
-          <Header>
-            <div className="ml-auto flex items-center justify-end space-x-4">
-              <SearchSidebar onSearch={() => {}} />
-              <ThemeSwitch />
-            </div>
-          </Header>
-          {/* Contenido principal (Outlet/Children) */}
-          <main className="flex-1 bg-gray-50 p-6 ">
+        <div className="flex-1 flex flex-col relative">
+          {/* Header arriba - con z-index alto */}
+          <div className="relative z-40">
+            <Header>
+              <div className="ml-auto flex items-center justify-end space-x-4">
+                <SearchSidebar onSearch={() => {}} />
+                <ThemeSwitch />
+              </div>
+            </Header>
+          </div>
+
+          {/* Contenido principal (Outlet/Children) - con z-index bajo */}
+          <main className="flex-1 bg-gray-50 p-6 relative z-0 overflow-auto">
             <Outlet />
           </main>
         </div>
